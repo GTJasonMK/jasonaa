5647bcdaf92ec8b74a6f3634227f8cd0a37adf97
milkway <2836777041@qq.com>
2025-05-22 17:52:34 +0800

    修改了死亡提示

--- snake.js
+++ snake.js
@@ -180,7 +180,7 @@ document.addEventListener('DOMContentLoaded', () => {
         ctx.font = '30px Microsoft YaHei';
         ctx.fillStyle = 'white';
         ctx.textAlign = 'center';
-        ctx.fillText('游戏结束!', canvas.width / 2, canvas.height / 2 - 30);
+        ctx.fillText('游戏结束，菜逼牛猪鸡!', canvas.width / 2, canvas.height / 2 - 30);
         
         ctx.font = '20px Microsoft YaHei';
         ctx.fillText(`最终得分: ${score}`, canvas.width / 2, canvas.height / 2 + 10);
