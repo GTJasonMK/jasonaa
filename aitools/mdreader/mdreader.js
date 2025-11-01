1720a9bb41a5d375a5edd6dc8401f68f46f1e280
milkway <2836777041@qq.com>
2025-11-01 11:55:07 +0800

    优化了对移动端的适配

--- aitools/mdreader/mdreader.js
+++ aitools/mdreader/mdreader.js
@@ -13,6 +13,7 @@ class MarkdownReader {
         this.markdownContent = document.getElementById('markdown-content');
         this.tocNav = document.getElementById('toc-nav');
         this.tocSidebar = document.getElementById('toc-sidebar');
+        this.tocOverlay = document.getElementById('toc-overlay');
         this.fileInfo = document.getElementById('file-info');
         this.fileName = document.getElementById('file-name');
 
@@ -90,6 +91,7 @@ class MarkdownReader {
         // 工具栏按钮
         this.toggleTocBtn.addEventListener('click', () => this.toggleToc());
         this.tocCloseBtn.addEventListener('click', () => this.toggleToc());
+        this.tocOverlay.addEventListener('click', () => this.toggleToc());
         this.copyAllBtn.addEventListener('click', () => this.copyAllContent());
         this.exportHtmlBtn.addEventListener('click', () => this.exportHtml());
         this.closeFileBtn.addEventListener('click', () => this.closeFile());
@@ -510,8 +512,10 @@ class MarkdownReader {
         this.tocVisible = !this.tocVisible;
         if (this.tocVisible) {
             this.tocSidebar.classList.remove('hidden');
+            this.tocOverlay.classList.add('active');
         } else {
             this.tocSidebar.classList.add('hidden');
+            this.tocOverlay.classList.remove('active');
         }
     }
 
